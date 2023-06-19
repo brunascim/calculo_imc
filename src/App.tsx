63961ee09f8632e0c73022cdc766403dee09775a
brunascim <127804993+brunascim@users.noreply.github.com>
2023-06-19 14:48:29 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-
-import { useState } from 'react';
-import styles from './App.module.css';
+import { useState } from 'react'
+import styles from './App.module.css'
 import poweredImage from './assets/powered.png'
+import { levels, calculateImc } from './helpers/imc'
+import { GridItem } from './components/GridItem'
+
 
 const App = () => {
 
-  const [altura, setAltura] = useState(0);
-  const [peso, setPeso]     = useState(0);
+  const [altura, setAltura]   = useState(0);
+  const [peso, setPeso]       = useState(0);
 
   const handleCalc = () => {
     if (altura && peso) {
@@ -16,42 +18,47 @@ const App = () => {
       alert("Informe a Altura e o Peso")
     }
   }
-  
-  return (
+
+  return(
     <div className={styles.main}>
-      <header> 
+      <header>
         <div className={styles.headerContainer}>
-          <img src={poweredImage} alt="" width={150} />
+          <img src={poweredImage} alt="" width={150}/>
         </div>
       </header>
-      <div className={styles.container}> 
+      <div className={styles.container}>
 
-          <div className={styles.leftSide}> 
-            <h1>Cálculo do IMC</h1>
-              <p> O IMC (Índice de Massa Corporal) é reconhecido pela OMS (Organização Mundial da Saúde) como um padrão internacional que avalia se as pessoas, entre 20 e 59 anos, estão com peso ideal ou em excesso, em relação a sua altura.</p>
-              <input 
-                type="number"
-                placeholder="Informe a altura. Ex. 1.5 (em metro)"
-                value={altura > 0 ? altura:''}
-                onChange={t => setAltura(parseFloat(t.target.value) )}
-                />
+           <div className={styles.leftSide}>
+             <h1>Cálculo do IMC</h1>
+             <p>O IMC (Índice de Massa Corporal) é reconhecido pela OMS (Organização Mundial da Saúde) como um padrão internacional que avalia se as pessoas, entre 20 e 59 anos, estão com peso ideal ou em excesso, em relação a sua altura. </p>
+             <input 
+               type="number"
+               placeholder="Informe a altura. Ex. 1.5 (em metro)"
+               value={altura > 0 ? altura:''}
+               onChange={t => setAltura(parseFloat (t.target.value) )}
+             />
 
-              <input 
-                type="number"
-                placeholder="Informe o peso . Ex. 60.5 (em kilo)"
-                value={peso > 0 ? peso:''}
-                onChange={t => setPeso(parseFloat(t.target.value) )}
-                />
+             <input 
+               type="number"
+               placeholder="Informe o peso. Ex. 60.5 (em kilo)"
+               value={peso > 0 ? peso:''}
+               onChange={t => setPeso(parseFloat (t.target.value) )}
+             />
 
-                <button onClick={handleCalc}>Calcular</button>
+             <button onClick={handleCalc}>Calcular</button>
 
-          </div>
+           </div>
+           
 
-          <div className={styles.rigthSide}>
-            ...
-          </div>
+           <div className={styles.rightSide}>
+              <div className={styles.grid}>
+                  {levels.map( (item, key) => (
+                    <GridItem key={key} item={item}/>
+                  ) )}
+            </div>
+
+           </div>
       </div>
-
     </div>
   )
 }
